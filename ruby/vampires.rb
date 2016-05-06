67e7fafb8c5375de9c766ff54fe86871b8a2fcc6
caitlinlikesrobots <caitlin.annette.johnson@gmail.com>
2016-05-06 11:31:03 -0500

    Modify conditional statements

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,41 +1,22 @@
 #HR Department: Vampire Detector Questionnaire
-system "clear"
+		system "clear"
 
 # Number of employees processed loop
-puts "How many employees are we processing today?"
-	employees = gets.to_i
-employees.times do 
-puts "What is your name?"
-	name = gets.chomp
-puts "How old are you?"
-	age = gets.to_i
-puts "What year were you born?"
-	birth_year = gets.to_i
-puts "Our company cafeteria serves delicious garlic bread. Should we order some for you? (yes/no)"
-	garlic_love = gets.chomp.downcase
-puts "Would you like to enroll in our company's health insurance? (yes/no)"
-	health = gets.chomp.downcase
+		puts "How many employees are we processing today?"
+			employees = gets.to_i
+		employees.times do 
+		puts "What is your name?"
+			name = gets.chomp
+		puts "How old are you?"
+			age = gets.to_i
+		puts "What year were you born?"
+			birth_year = gets.to_i
+		puts "Our company cafeteria serves delicious garlic bread. Should we order some for you? (yes/no)"
+			garlic_love = gets.chomp.downcase
+		puts "Would you like to enroll in our company's health insurance? (yes/no)"
+			health = gets.chomp.downcase
 
-#Check for allergies loop
-puts "Do you have any allergies? (yes or no)"
-	allergies = gets.chomp.downcase
-if allergies == "yes"
-	puts "Please enter your allergies one at a time. Press enter after each allergy. Write 'done' when finsihed"
-else puts "No allergies? Lucky! Write 'done' now."
-end
-	allergy = nil
-	allergy = gets.chomp
-while allergy != "done"
-	allergy = gets.chomp.downcase
-	if allergy == "sunshine"
-		puts = "Probably a vampire. Careful!"
-		break
-	else puts "Name your allergy."
-	
-end
-end
-
-		current_year = 2016
+current_year = 2016
 #Check for correct age
 		correct_age = current_year - birth_year
 		if correct_age == age 
@@ -44,21 +25,21 @@ end
 			correct_age = false
 		end 
 
-		#Check for garlic preference
+#Check for garlic preference
 		if garlic_love == "yes"
 			garlic_love = true
 		else garlic_love == "no"
 			garlic_love = false
 		end
 
-		#Check for health insurance preference
+#Check for health insurance preference
 		if health == "yes"
 			health = true
 		else health == "no"
 			health = false
 		end
 
-		#Vampire Detection Logic
+#Vampire Detection Logic
 		if name == "Drake Ula" || "Tu Fang"
 			vampire_detect = "Definitely a vampire. Get the stake immediately."
 		elsif  correct_age && garlic_love && health
@@ -67,19 +48,37 @@ end
 			vampire_detect = "Probably a vampire. Proceed with caution."
 		elsif !correct_age && !garlic_love && !health 
 			vampire_detect = "Almost certainly a vampire. Prepare the silver."
-		else puts "Results inconclusive."
+		else vampire_detect = "Results inconclusive."
+		puts vampire_detect
 		end
-	print vampire_detect
 
-	
+#Check for allergies loop
+			puts "Do you have any allergies? (yes or no)"
+			allergies = gets.chomp.downcase
+		if allergies == "yes"
+			puts "Please enter your allergies one at a time. Press enter after each allergy. Write 'done' when finsihed"
+		else puts "No allergies? Lucky! Write 'done' now."
+		end
 
-print	"Employee name: #{name}""\n"
-print 	"Employee age: #{age}""\n"
-print	"Employee birth year: #{birth_year}""\n"
-print	"Employee garlic preference: #{garlic_love}""\n"
-print	"Employee health preference: #{health}""\n"
-	end
+			allergy = nil
+		while allergy != "done"
+			puts "Name your allergy."
+			allergy = gets.chomp.downcase
+		if 	allergy == "sunshine"
+			vampire_detect= "Probably a Vampire. Be careful!"
+		puts vampire_detect
+			break
+		end
+		end
+
+
+		print	"Employee name: #{name}""\n"
+		print 	"Employee age: #{age}""\n"
+		print	"Employee birth year: #{birth_year}""\n"
+		print	"Employee garlic preference: #{garlic_love}""\n"
+		print	"Employee health preference: #{health}""\n"
+		end
 
 
 
-puts "Actually, this is all completely ridiculous. Vampires? That's bonkers. Let's go grab a drink. \n OF BLOOD!"
+		puts "Actually, this is all completely ridiculous. Vampires? That's bonkers. Let's go grab a drink. \n OF BLOOD!"
