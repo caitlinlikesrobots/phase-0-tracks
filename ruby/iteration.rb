d2343e5eab7dbd1627925933e2c842aa9c72bd72
caitlinlikesrobots <caitlin.annette.johnson@gmail.com>
2016-05-10 17:40:23 -0500

    Delete even integers from pain_level

---
+++ ruby/iteration.rb
@@ -0,0 +1,32 @@
+def we_say_stuff
+	puts "WAZZUP!!!!!!!!!"
+	yield("Floppy Mc'Wobbletons")
+end
+
+we_say_stuff { |name| puts "great to see you #{name}"}
+
+zoo = ["walrus", "penguin", "polar bear", "human"]
+movie = { "scifi" => "Bladerunner", "horror" => "Army of Darkness", "comedy" => "Duck Soup", "drama" => "Gladiator" }
+
+zoo.each do |animal|
+	puts "There is a #{animal} in the zoo."
+end
+
+movie.each do |genre, movie|
+	puts "#{movie} is a #{genre} movie."
+end
+
+p zoo
+
+zoo.map! do |animal|
+	animal.prepend("Evil ")
+end
+
+p zoo
+
+
+pain_level = [1, 2, 3, 4, 5]
+pain_level.delete_if { |pain| pain.even? }
+p pain_level
+
+ascii_char_hash = {"A" => 65, "B" => 66, "C" => 67}
