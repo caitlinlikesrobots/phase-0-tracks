2345e260d21f34e6f4aee30856f6f5ad91bb045e
caitlinlikesrobots <caitlin.annette.johnson@gmail.com>
2016-05-10 15:56:30 -0500

    Create method to return array

---
+++ ruby/arrays_drill.rb
@@ -0,0 +1,22 @@
+def inventory(arr) 
+	return arr
+end
+
+
+store = []
+
+store << "candy" << "gum" << "chips" << "soda" << "ice"
+
+store.delete_at(2)
+
+store.insert(2, "burrito")
+
+store.delete("candy")
+
+store.include?("gum")
+"it is #{store.include?("gum")} that this store includes gum"
+
+rival_store = ["cigarettes", "beer", "wine"]
+
+# p rival_store + store
+p inventory(["candy", "gum", "chips"])
