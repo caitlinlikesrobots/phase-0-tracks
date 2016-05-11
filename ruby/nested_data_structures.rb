442788bad3ceb7b1d24c678a0459fc99f12e0961
caitlinlikesrobots <caitlin.annette.johnson@gmail.com>
2016-05-11 12:19:17 -0500

    Initial nested structure

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,17 @@
+# A Mansion Filled with Guests, Ghosts, and SpooOooky Times!
+
+mansion{
+	Guests[
+		"Charlamagne Money",
+		"Kristoff Belvedere",
+		"Jens Starsgaard",
+		"Caitlin Johnson"
+	]
+
+	Ghosts{
+		ghost1 => "The Exhausted Mailman",
+		ghost2 => "The Heartbroken Knight",
+		ghost3 => "The Forlorn Widow",
+		ghost4 => "The Rebellious Teenager",
+	}
+}
