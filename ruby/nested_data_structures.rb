798bf664c0b546ec4cecc5689def66fa266cf9a0
caitlinlikesrobots <caitlin.annette.johnson@gmail.com>
2016-05-11 13:33:32 -0500

    Give value to nested hash

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,17 +1,38 @@
 # A Mansion Filled with Guests, Ghosts, and SpooOooky Times!
 
-mansion{
-	Guests[
-		"Charlamagne Money",
-		"Kristoff Belvedere",
-		"Jens Starsgaard",
-		"Caitlin Johnson"
-	]
+mansion = {
 
-	Ghosts{
-		ghost1 => "The Exhausted Mailman",
-		ghost2 => "The Heartbroken Knight",
-		ghost3 => "The Forlorn Widow",
-		ghost4 => "The Rebellious Teenager",
-	}
+	Guests: {
+		guest1: "Charlamagne Money",
+		guest2: "Kristoff Belvedere",
+		guest3: "Jens Starsgaard",
+	},
+
+	Ghosts: {
+		ghost1: {
+				name:"Henry", 
+				character:"The Friendly Mailman", 
+				age_at_demise:94,
+				year_of_demise:1937,
+				activity:"Fetches the mail. From the entire neighborhood.",
+			   },
+
+		ghost2: {
+				name:"Susie", 
+				character:"The Laughing Child", 
+				age_at_demise:7,
+				year_of_demise:1864,
+				activity: "Laughs at any given time. It is a bit creepy.",
+			   },
+		
+		ghost3: {
+				name:"Dylan", 
+				character:"The Rebellious Teenager", 
+				age_at_demise:18,
+				year_of_demise:"Unwilling to say.",
+				activity:"Smokes ghost cigarettes and can often be heard saying 'phonies' regarding guests.",
+			   },
+			},
 }
+
+p mansion
