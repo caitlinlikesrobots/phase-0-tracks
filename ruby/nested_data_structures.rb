388e7dd9275ddca328996530b55cc1d01d704d1c
caitlinlikesrobots <caitlin.annette.johnson@gmail.com>
2016-05-11 13:47:03 -0500

    Access nested data v.1

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -2,37 +2,42 @@
 
 mansion = {
 
-	Guests: {
-		guest1: "Charlamagne Money",
-		guest2: "Kristoff Belvedere",
-		guest3: "Jens Starsgaard",
-	},
+	guests: [
+		"Charlamagne Money", 
+		"Jessica Lightingale",
+		"Kristoff Belvedere", 
+		"Gordon Gilmore",
+		"Jens Starsgaard", 
+		"His Pet Tiger"
+	],
 
-	Ghosts: {
+	ghosts: {
 		ghost1: {
-				name:"Henry", 
-				character:"The Friendly Mailman", 
-				age_at_demise:94,
-				year_of_demise:1937,
-				activity:"Fetches the mail. From the entire neighborhood.",
+				name: "Henry", 
+				character: "The Friendly Mailman", 
+				age_at_demise: 94,
+				year_of_demise: 1937,
+				activity: "Fetches the mail. From the entire neighborhood.",
 			   },
 
 		ghost2: {
-				name:"Susie", 
-				character:"The Laughing Child", 
-				age_at_demise:7,
-				year_of_demise:1864,
+				name: "Susie", 
+				character: "The Laughing Child", 
+				age_at_demise: 7,
+				year_of_demise: 1864,
 				activity: "Laughs at any given time. It is a bit creepy.",
 			   },
 		
 		ghost3: {
-				name:"Dylan", 
-				character:"The Rebellious Teenager", 
+				name: "Dylan", 
+				character: "The Rebellious Teenager", 
 				age_at_demise:18,
-				year_of_demise:"Unwilling to say.",
-				activity:"Smokes ghost cigarettes and can often be heard saying 'phonies' regarding guests.",
+				year_of_demise: "Unwilling to say.",
+				activity: "Smokes ghost cigarettes and can often be heard saying 'phonies' regarding guests.",
 			   },
 			},
 }
 
-p mansion
+# p mansion
+
+p mansion [:guests][1]
