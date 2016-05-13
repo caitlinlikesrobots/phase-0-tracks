0f36ffb8ef3e551731f9bcf066c61e99a15d96d4
caitlinlikesrobots <caitlin.annette.johnson@gmail.com>
2016-05-13 13:56:14 -0500

    Modify alias code

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -6,82 +6,66 @@
 	* in this instance, y will be a consonant
 =end
 
-# first_name = gets.chomp.downcase
-# last_name = gets.chomp.downcase
-# name = first_name + last_name
-# alias = last_name + first_name
+# first_name = gets.chomp.downcase and made into array
+# last_name = gets.chomp.downcase and made into array
+
+
+#vowels needs to be a global variable here due to the frequency 
+#of its use
+$vowels = "aeiou"
+consonants = "bcdfghjklmnpqrstvwxyz"
 
 #moves vowels forward one character
-def new_vowels
-	vowels = "aeiou".split('')
-	vowels.class
-	new_vowels = vowels.rotate!
+def new_vowels(letter)
+	$vowels = "aeiou".split('')
+	$vowels.class
+	new_vowels = $vowels.rotate!
+	new_vowels.join('')
+	
 end
 
-# puts new_vowels
-
 #moves consonants forward one character
-def new_consonants
+def new_consonants(letter)
 	consonants = "bcdfghjklmnpqrstvwxyz".split('')
 	consonants.class
 	new_consonants = consonants.rotate!
+	new_consonants.join('')
+	
 end
 
-# puts new_consonants
+#combines the new_vowels and new_consonants methods for use
+
+def alter_letters(letter_array)
+	letter_array.map! { |letter|
+		$vowels.include?(letter) ? letter = new_vowels(letter)
+				 :
+		
+	letter_array.map! {|letter| letter = new_consonants(letter)
+				 }
+
+		# name[0].upcase!
+		alias_array = letter_array.join('')}
+			 
+end
 
 #Generate new names
-screen = "clear"
 
-
-puts "Hello agent. What is your first and last name?"
-name = gets.chomp.downcase.split(" ")
-
+def alias_generator
+	puts "Hello agent. What is your first and last name?"
+# first_name = gets.chomp.downcase and made into array
+# last_name = gets.chomp.downcase and made into array
+	name = gets.chomp.downcase.split(" ")
 #swaps first and last names
-name.shuffle!
+	name.reverse!
+#separates last and first names and split into arrays
+	first_name = name[0].split('')
+	last_name = name[1].split('')
+#provides new alias with alter_letters method
+	new_alias = "#{alter_letters(first_name)} #{alter_letters(last_name)}"
+	yield(new_alias)
+end
+alias_generator {|new_alias| puts "Your new alias is #{new_alias}."}
 
-
-# def alias_generator(name)
-# i = 0
-# new_vowels
-# new_consonants
-# while i < name.length
-# 	if name[i] == " "
-
-# 	end
-
-# 	i += 1
-# end
-# 	return name
-# end
-
-# puts "Hello agent. What is your first and last name?"
-# name = gets.chomp.downcase.split(" ")
-
-# #swaps first and last names
-# name.rotate!
-# name.class
-# new_vowels(name)
-# new_consonants(name)
-# swap_name = name.join(" ")
-
-# alias_generator(name)
-
-
-
-
-
-
-
-# name = gets.chomp.chars.map(&:downcase)
-# p name
-# name.class
-
-
-# name_arr = []
-#  while name != "done"
-#  	name_arr << name.to_i
-#  	name = gets.chomp
-#  end
 
 
 
