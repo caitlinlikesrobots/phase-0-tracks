9667ed4142f30c1552dfcd513e704864f49b1f65
caitlinlikesrobots <caitlin.annette.johnson@gmail.com>
2016-05-16 12:40:36 -0500

    Add new puppy species

---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+SPECIES ------------------------
+Canis lateus amplexus
+
+CHARACTERISTICS ----------------
+Ability to hug: varies
+Friendly: yes
+Eye count: 2
+Tail count: 1
+Wag_tail: varies
+Name: varies
+Color: varies
+
+BEHAVIOR -----------------------
+Jump
+Hug
+Frisbee allstar
