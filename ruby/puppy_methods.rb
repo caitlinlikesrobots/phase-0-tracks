2d74747cbc0d2cf29e1c1583830c90a6147a4c28
caitlinlikesrobots <caitlin.annette.johnson@gmail.com>
2016-05-16 15:49:18 -0500

    Final puppy methods and begin release 2

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,8 +1,28 @@
 class Puppy
 
+  def initialize
+  	puts "Initializing new puppy instance..."
+  end
+
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
+  end
+
+  def speak(i)
+  	i.times { puts "Woof!" }
+  end
+
+  def roll_over
+  	puts "*rolls over*"
+  end 
+
+  def dog_years(human_years)
+  	puts human_years / 7
+  end
+
+  def sit_pretty
+  	puts "I am sitting pretty"
   end
 
 end
@@ -11,4 +31,21 @@ end
 scruffy = Puppy.new
 
 # verification of Scruffy's ability to fetch
-p scruffy.fetch("sock")
+scruffy.fetch("sock")
+scruffy.speak(3)
+scruffy.roll_over
+scruffy.dog_years(50)
+scruffy.sit_pretty
+
+
+# Release 2
+
+class Robot_Army
+
+	def initialize
+		puts "Initializing new robot army instance..."
+  	end
+end
+
+
+
