760cb9daa842a76066b7253e6c7f4b975d58c01f
caitlinlikesrobots <caitlin.annette.johnson@gmail.com>
2016-05-17 14:46:53 -0500

    Complete UI for perfect pet

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -12,13 +12,13 @@ Create Perfect Pet User App to make your pet truly Perfect!
 
 class Perfect_Pet 
 	attr_reader :color
-	attr_accessor :name, :description
+	attr_accessor :name, :birthday
 	def initialize(name)
 	@name = name
 	@color = color
-	@description = description
+	@birthday = birthday
 
-	puts "Congratulations! Your Perfect Pet ™ is named #{@name}, and it loves you! \n Love!"
+	puts "Congratulations! Your Perfect Pet™ is named #{@name}, and it loves you! \n Love!"
 	end
 
 	def hug
@@ -28,7 +28,6 @@ class Perfect_Pet
 	def high_five
 		puts "You hold up your hand and #{@name} holds up its soft, soft paw. \n Your appendages connect! \n Friendship!"
 	end
-
 
 	def feed
 		pet_hungry = ""
@@ -46,8 +45,50 @@ class Perfect_Pet
 	end
 end
 
-p pet = Perfect_Pet.new("Harold")
-p pet.hug
-p pet.high_five
-p pet.feed
+# UI *-------------------------------------------------------* UI
+
+#Welcomes the user to the Perfect Pet App
+
+puts "Welcome to Perfect Pet™ headquarters!\n We are here to guide you through making\n your Perfect Pet™!"
+
+#prompts user to name their first pet
+
+puts "What would you like to name your Perfect Pet™?"
+@name = gets.chomp
+p pet = Perfect_Pet.new(@name)
+
+#prompts user to choose a Perfect Pet color 
+
+puts "What color would you like your Perfect Pet™ to be?"
+@color = gets.chomp.downcase
+puts "Wow look at how #{@color} #{@name} is! Great choice!"
+
+#gives the user their Perfect Pet's exact birthday
+@birthday = Time.now
+puts "#{@name}'s birthday is #{@birthday}! Don't forget to get it a gift.\n We also hear it loves parties!"
+
+#walks user through Perfect Pet abilities
+
+puts "Would you like to hug #{@name}? (enter yes or no)"
+	hug = gets.chomp
+	if hug == "yes"
+	p pet.hug
+	else puts "That is ok. \nHugs are pretty intimate for a pet you just met."
+	end
+
+puts "Would you like to high-five #{@name}? (enter yes or no)"
+	fiver = gets.chomp
+	if fiver == "yes"
+	p pet.high_five
+	else puts "That is ok. \nHigh-fives are difficult to master.\nIt is all in the elbow!"
+	end
+puts "Would you like to feed #{@name}? (enter yes or no)"
+	feed_pet = gets.chomp
+	if feed_pet == "yes"
+	p pet.feed
+	else puts "That is ok. #{@name} ate earlier."
+	end 
+
+
+
 
