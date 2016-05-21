47164d49a544bda558d4e85650cd08d278dec2df
Caitlin Johnson <caitlin.annette.johnson@gmail.com>
2016-05-21 00:36:00 -0500

    Update solo.rb

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -9,15 +9,14 @@ Perfect Pet will also have three methods:
 	*high-five your Perfect Pet
 Create Perfect Pet User App to make your pet truly Perfect!
 =end
-# Clear screen
-system 'clear'
-#Create Array for Perfect Pet information
-$perfect_pets = Array.new
 
 # Create Perfect Pet class
 class Perfect_Pet 
-	attr_reader :color, :birthday
-	attr_accessor :name 
+	attr_reader :birthday
+	attr_accessor :name, :color
+
+	class << self
+	
 	def initialize(name)
 	@name = name
 	@color = color
@@ -52,71 +51,67 @@ end
 
 # UI *-------------------------------------------------------* UI
 
+
+
+# ßerfect_pets = []
+
+loop do
 #Welcomes the user to the Perfect Pet App
-def pet_creator
 puts "Welcome to Perfect Pet™ headquarters!\n We are here to guide you through making\n your Perfect Pet™!"
 
 #prompts user to name their first pet
 
 puts "What would you like to name your Perfect Pet™?"
 @name = gets.chomp
-p pet = Perfect_Pet.new(@name)
-$perfect_pets << @name
-
+pet = Perfect_Pet.new(@name)
 #prompts user to choose a Perfect Pet color 
 
 puts "What color would you like your Perfect Pet™ to be?"
 @color = gets.chomp.downcase
 puts "Wow look at how #{@color} #{@name} is! Great choice!"
-$perfect_pets << @color
 
 #gives the user their Perfect Pet's exact birthday
 
 @birthday = Time.now
 puts "#{@name}'s birthday is #{@birthday}! Don't forget to get it a gift.\n We also hear it loves parties!"
-$perfect_pets << @birthday
 
 #walks user through Perfect Pet abilities
 
 puts "Would you like to hug #{@name}? (enter yes or no)"
-	hug = gets.chomp.downcase
-	if hug == "yes"
-	p pet.hug
+	hugs = gets.chomp.downcase
+	if hugs == "yes"
+	pet.hug
 	else puts "That is ok. \nHugs are pretty intimate for a pet you just met."
 	end
 
 puts "Would you like to high-five #{@name}? (enter yes or no)"
 	fiver = gets.chomp.downcase
 	if fiver == "yes"
-	p pet.high_five
+	pet.high_five
 	else puts "That is ok. \nHigh-fives are difficult to master.\nIt is all in the elbow!"
 	end
 puts "Would you like to feed #{@name}? (enter yes or no)"
 	feed_pet = gets.chomp.downcase
 	if feed_pet == "yes"
-	p pet.feed
+	pet.feed
 	else puts "That is ok. #{@name} ate earlier."
 	end 
-end
-
-p pet_creator
 
 #Asks user if they would like to make another Perfect Pet
 #IF yes, call pet_creator again
-puts "Would you like to create another Perfect Pet™? (enter yes or no)"
-	make_pet = gets.chomp.downcase
-	if make_pet == "yes"
-		p pet_creator
-	else puts "Thank you so much.\nEnjoy your Perfect Pet™!"
-	end
-
-#------------* Prints Pets Made *------------
-
-system 'clear'
-$perfect_pets.each do |pet|
-	puts "Your Perfect Pet is #{@name} who is #{@color} \nand was born on #{@birthday}!"
+puts "Would you like to create another Perfect Pet™? \n(Enter 'yes' to make a new pet or enter 'quit' to exit.)"
+	pet = gets.chomp.downcase
+	if pet != "quit"
+	else puts "Thank you very much.\nEnjoy your Perfect Pet™!"
+	   break
+	end	 
 end
 
+#------------* Prints Pets Made *----------
+system 'clear'
+Perfect_Pet do |pet|
+puts "Your Perfect Pet is #{@name} who is #{@color}\n and their birthday is #{@birthday}!"
+end
 
 
 
