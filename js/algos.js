a1b993c9cbc70b46c4cd732805244cfdb36e6f19
caitlinlikesrobots <caitlin.annette.johnson@gmail.com>
2016-05-30 12:36:29 -0500

    Create function for longest work

--- js/algos.js
+++ js/algos.js
@@ -5,16 +5,43 @@
 
 var funWords = ["owl", "pleasant", "criticaster", "futz"];
 
-// create variables for general length and longest length for comparison 
-var lngth = 0;
-var longestWord; 
+
+// function LongestWord(funWords){
+// I thought this might print the array
+// console.log(this); 
+// this.array = funWords;
+// console.log(funWords[1]);
+}
 
 // create a loop that will iterate through the array and return the longest word
 
-for(var i=0; i < funWords.length; i++){
-	if(funWords.index.length > lngth){
-		var lngth = funWords.index.length;
-		longestWord = funWords.index;
+// for(var i=0; i < funWords.length; i++){
+// 	if(funWords.index.length > lngth){
+// 		var lngth = funWords.index.length;
+// 		longestWord = funWords.index;
+// 	}
+// }
+
+//.index doesn't seem to be an option...
+// create variable for general length and function for longest length for comparison 
+function longestWord(funWords) {
+// sets up a loop that moves through the length of the array
+// var lngth = 0;
+findLongWord: function() {
+	for(var i = 0; i < funWords.length; i++){
+	// conditional statement that moves through the index [i] lengths
+	// through the use of the index, move through array
+	// with comparative statement, lngth variable becomes the length of 
+	// the strings in array
+	// longest word becomes the string at the index that represents the longest
+	// string of characters based on the .length method
+	if(funWords[i].length > 0){
+		lngth = funWords[i].length;
+		longestWord = funWords[i];
 	}
+  }
 }
-console.log(longestWord);
+};
+// console.log(longestWord); 
+// OH MAN IT WORKED now I have to make it a function...
+longestWord.findLongWord();
