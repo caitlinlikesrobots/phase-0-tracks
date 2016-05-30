15fb3c77ddd374bddf3e6f2fe5d45bba89d8d91b
caitlinlikesrobots <caitlin.annette.johnson@gmail.com>
2016-05-30 13:29:22 -0500

    Add function and indices to longeststring

--- js/algos.js
+++ js/algos.js
@@ -1,17 +1,49 @@
+// Longest String in An Array Function
 // create an array of words with various lengths 
-// use a function that counts character length for strings
-// iterate through this array with character count method
+// use a FUNCTION that counts character length for EACH string
+// ITERATE through this array with character count method
+// IF string is longer than the string before it, put it in a new array of longWords
+// make multiple longest strings FALSE
 // return the longest string 
 
-var funWords = ["owl", "pleasant", "criticaster", "futz"];
+function LongestWord(array) {
+	var longestString = 0;
+	var lngth = 0;
+	var longestWord = [];
+	var manyLongWords = false;
+	
 
+	for(var i = 0; i < funWords.length; i++){
+	// conditional statement that moves through the index [i] lengths
+	// through the use of the index, move through array
+	// with comparative statement, lngth variable becomes the length of 
+	// the strings in array
+	// longest word becomes the string at the index that represents the longest
+	// string of characters based on the .length method
+			if(funWords[i].length > 0){
+			lngth = funWords[i].length;
+			longestWord = funWords[i];
+			manyLongWords = false;
+	}
+	// account for words with the same longest length
+			else if (funWords[i].length == longestString){
+				manyLongWords = true;
+				longestWord = funWords[i];
+			};
+	};
+  
+
+
+//-------------------------DRIVER CODE-------------------------//
+console.log(funWords(["fruit","basketball","highest","penultimate","mark"]));
+console.log(funWords(["egg","fun"]));
 
 // function LongestWord(funWords){
 // I thought this might print the array
 // console.log(this); 
 // this.array = funWords;
 // console.log(funWords[1]);
-}
+// }
 
 // create a loop that will iterate through the array and return the longest word
 
@@ -24,24 +56,28 @@ var funWords = ["owl", "pleasant", "criticaster", "futz"];
 
 //.index doesn't seem to be an option...
 // create variable for general length and function for longest length for comparison 
-function longestWord(funWords) {
-// sets up a loop that moves through the length of the array
-// var lngth = 0;
-findLongWord: function() {
-	for(var i = 0; i < funWords.length; i++){
-	// conditional statement that moves through the index [i] lengths
-	// through the use of the index, move through array
-	// with comparative statement, lngth variable becomes the length of 
-	// the strings in array
-	// longest word becomes the string at the index that represents the longest
-	// string of characters based on the .length method
-	if(funWords[i].length > 0){
-		lngth = funWords[i].length;
-		longestWord = funWords[i];
-	}
-  }
-}
-};
+// function longestWord(funWords) {
+// // sets up a loop that moves through the length of the array
+// // var lngth = 0;
+// findLongWord: function() {
+// 	for(var i = 0; i < funWords.length; i++){
+// 	// conditional statement that moves through the index [i] lengths
+// 	// through the use of the index, move through array
+// 	// with comparative statement, lngth variable becomes the length of 
+// 	// the strings in array
+// 	// longest word becomes the string at the index that represents the longest
+// 	// string of characters based on the .length method
+// 	if(funWords[i].length > 0){
+// 		lngth = funWords[i].length;
+// 		longestWord = funWords[i];
+// 	}
+//   }
+// }
+// };
+
+// create a FUNCTION that takes two objects and checks see if they share a key-value pair
+//
+
 // console.log(longestWord); 
-// OH MAN IT WORKED now I have to make it a function...
-longestWord.findLongWord();
+// OH MAN IT WORKED but now I have to make it a function...
+// longestWord.findLongWord(); //this did not work
