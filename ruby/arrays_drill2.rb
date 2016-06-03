0d7396515c488ac600ee8f7aa52cd30db196a7e0
caitlinlikesrobots <caitlin.annette.johnson@gmail.com>
2016-06-03 16:01:36 -0500

    Add new array and delete array at index 2

--- ruby/arrays_drill2.rb
+++ ruby/arrays_drill2.rb
@@ -2,5 +2,13 @@ new_array = []
 p new_array
 
 new_array << "J.K. Rowling" << "George R. R. Martin" << "Suzanne Collins" << "Stephanie Meyer" << "J. R. R. Tolkein"
+p new_array
 
+# new_array.delete_at(2)
+# p new_array
+
+# new_array.insert(2, "Brian Sanderson")
+# p new_array
+
+new_array.delete("J.K. Rowling")
 p new_array
