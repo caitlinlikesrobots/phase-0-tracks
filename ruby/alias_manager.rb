695c8dacc9ebfd0e43fb8a91651256606fbfc54f
caitlinlikesrobots <caitlin.annette.johnson@gmail.com>
2016-06-03 14:08:29 -0500

    Debug translate 'quit'

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -52,11 +52,8 @@ agent_name = ''
 while agent_name != 'quit'
 puts "Hello agent. What names would you like processed? Enter 'quit' to finish"
 	agent_name = gets.chomp
-		# ends the loop 
-	p aliases[agent_name.to_sym] = alias_generator(agent_name)
 	if agent_name != 'quit'
 		aliases[agent_name.to_sym] = alias_generator(agent_name)
-		alias_generator(agent_name)
 	else 
 		p "Thank you!"
 	end
@@ -65,7 +62,7 @@ end
 
 
 # final printing hash (NEED HELP WITH THIS!)
-aliases.each { |agent_name, new_alias| puts "puts #{agent_name} is now #{new_alias}."}
+aliases.each { |agent_name, new_alias| puts "#{agent_name} is now #{new_alias}."}
 
 
 
