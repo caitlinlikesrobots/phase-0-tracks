7c8d1fda774ee2cb1c8f328802c1d8fad931f271
caitlinlikesrobots <caitlin.annette.johnson@gmail.com>
2016-06-22 15:36:46 -0500

    Initial commit

---
+++ ruby/bucketlist.rb
@@ -0,0 +1,20 @@
+# BucketList
+=begin 
+* Allow users to CREATE their BucketList profile 
+* Allow users to READ other users BucketList profiles
+* Allow users to UPDATE their information
+* Allow users to DELETE profile
+=end
+
+#require gems
+require 'sqlite3'
+require 'faker'
+
+# Create SQLite3 database 
+db = db = SQLite3::Database.new("csa.db")
+
+# Create table for user information (id, name, age, instagram, facebook, twitter, activity_id)
+
+# Create table for user bucket list information (id, activity, accomplished?)
+
+# Create UI for user input 
