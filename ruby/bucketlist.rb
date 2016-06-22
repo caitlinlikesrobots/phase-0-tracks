4e22f0352c92cd1e61ba8966004c5d0420c3b3f2
caitlinlikesrobots <caitlin.annette.johnson@gmail.com>
2016-06-22 16:03:00 -0500

    Create database for ruby project

--- ruby/bucketlist.rb
+++ ruby/bucketlist.rb
@@ -11,10 +11,41 @@ require 'sqlite3'
 require 'faker'
 
 # Create SQLite3 database 
-db = db = SQLite3::Database.new("csa.db")
+db = SQLite3.Database.new("bucket.db")
+db.results_as_hash = true
 
-# Create table for user information (id, name, age, instagram, facebook, twitter, activity_id)
-
+Create table for user information (id, name, age, instagram, location by zipcode, FOREIGN KEY activity_id)
+create_users_cmd = <<-SQL
+	CREATE TABLE IF NOT EXISTS users(
+		id INTEGER PRIMARY KEY,
+		name VARCHAR(255),
+		age INT,
+		instagram VARCHAR(255),
+		activity VARCHAR(255)
+		location INT,
+		FOREIGN KEY (activity) REFERENCES activites(id),
+		FOREIGN KEY (location) REFERENCES locations(id)
+	);
+SQL
 # Create table for user bucket list information (id, activity, accomplished?)
+create_bucketlist_cmd = <<-SQL
+	CREATE TABLE IF NOT EXISTS bucketlist(
+		id INTEGER PRIMARY KEY,
+		activity VARCHAR(255),
+		accomplished BOOLEAN,
+	);
+SQL
 
+# Create table for user location 
+create_location_cmd = <<-SQL
+	CREATE TABLE IF NOT EXISTS location(
+		id INTEGER PRIMARY KEY,
+		location INT,
+	);
+SQL
+
+# Create tables with commands
+db.execute(create_users_cmd)
+db.execute(create_location_cmd)
+db.execute(create_bucketlist_cmd)
 # Create UI for user input 
