3cbf97bc33738e5fd5abce0009d21b2a08f94a01
caitlinlikesrobots <caitlin.annette.johnson@gmail.com>
2016-06-25 14:39:21 -0500

    Add functional UI and update tables for meaning

--- databases/summer_goal/summergoal.rb
+++ databases/summer_goal/summergoal.rb
@@ -21,19 +21,20 @@ create_users_cmd = <<-SQL
 		age INT,
 		zipcode INT,
 		instagram VARCHAR(255),
-		activity_id INT,
+		activity VARCHAR(255),
+		comment VARCHAR(255),
+		activity_location INT,
+		motivation_id INT,
 		accomplished_id INT,
-		FOREIGN KEY (activity_id) REFERENCES summergoal(id),
+		FOREIGN KEY (motivation_id) REFERENCES motivation(id),
 		FOREIGN KEY (accomplished_id) REFERENCES accomplished(id)
 	);
 SQL
 # Create activities table for user summer goal information (id, activity, accomplished?)
-create_summergoal_cmd = <<-SQL
-	CREATE TABLE IF NOT EXISTS summergoal(
+create_motivation_cmd = <<-SQL
+	CREATE TABLE IF NOT EXISTS motivation(
 		id INTEGER PRIMARY KEY,
-		activity VARCHAR(255),
-		comment VARCHAR(255),
-		location INT
+		motivation VARCHAR(255)
 	);
 SQL
 
@@ -48,18 +49,18 @@ SQL
 # Create tables with commands
 db.execute(create_users_cmd)
 db.execute(create_accomplished_cmd)
-db.execute(create_summergoal_cmd)
+db.execute(create_motivation_cmd)
 
 # Create methods for CRUD
 
 # create a method to create a user
-def create_user(db, name, age, zipcode, activity_id, social_id)
-	db.execute("INSERT INTO users (name, age, zipcode, activity_id, accomplished_id) VALUES (?, ?, ?, ?, ?)", [name, age, zipcode, activity_id, accomplished_id])
+def create_user(db, name, age, zipcode, instagram, activity, comment, activity_location, motivation_id, accomplished_id)
+	db.execute("INSERT INTO users (name, age, zipcode, instagram, activity, comment, activity_location, motivation_id, accomplished_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", [name, age, zipcode, instagram, activity, comment, activity_location, motivation_id, accomplished_id])
 end
 
 # create a method to add activities
-def create_summergoal(db, activity, comment, location)
-	db.execute("INSERT INTO summergoal (activity, comment, location) VALUES (?, ?, ?, ?)", [activity, comment, location])
+def create_motivation(db, motivation)
+	db.execute("INSERT INTO motivation (motivation) VALUES (?)", [motivation])
 end
 
 # create a method to add social media usernames
@@ -74,20 +75,21 @@ def display_user(db)
 		SELECT users.name, 
 		users.age, 
 		users.zipcode,
-		users.instagram, 
-		accomplished.accomplished, 
-		summergoal.activity, 
-		summergoal.comment, 
-		summergoal.location 
+		users.instagram,  
+		users.activity, 
+		users.comment, 
+		users.activity_location,
+		accomplished.accomplished,
+		motivation.motivation 
 		FROM users 
-		INNER JOIN summergoal 
-		ON users.activity_id = summergoal.id 
+		INNER JOIN motivation 
+		ON users.motivation_id = motivation.id 
 		INNER JOIN accomplished 
 		ON users.accomplished_id = accomplished.id;
 	SQL
 	display = db.execute(display_users_cmd)
 	display.each do |user|
-		puts "Name: #{user["name"]} \nAge: #{user["age"]} \nLocation: #{user["zipcode"]} \nSummer Goal: #{user["activity"]} \nActivity Comment: #{user["comment"]} \nInstagram: #{user["instagram"]} \nActivity Location: #{user["location"]} \nAccomplished:#{user["accomplished"]}"
+		puts "--------------\nName: #{user["name"]} \nAge: #{user["age"]} \nLocation: #{user["zipcode"]} \nSummer Goal: #{user["activity"]} \nActivity Comment: #{user["comment"]} \nInstagram: #{user["instagram"]} \nActivity Location: #{user["activity_location"]} \nAccomplished:#{user["accomplished"]} \n#{user["motivation"]}"
 	end
 end
 
@@ -97,7 +99,7 @@ end
 		name_cmd = "UPDATE users SET name = ? WHERE name = ?"
 		age_cmd = "UPDATE users SET age = ? WHERE age = ?"
 		zipcode_cmd = "UPDATE users SET zipcode = ? WHERE zipcode = ?"
-		activity_id_cmd = "UPDATE users SET activity_id = ? WHERE activity_id = ?"
+		motivation_id_cmd = "UPDATE users SET motivation_id = ? WHERE motivation_id = ?"
 		accomplished_id_cmd = "UPDATE users SET accomplished_id = ? WHERE accomplished_id = ?"
 
 		case updated_object
@@ -107,8 +109,8 @@ end
 			cmd = age_cmd
 		when 'zipcode'
 			cmd = zipcode_cmd
-		when 'activity_id'
-			cmd = activity_id_cmd
+		when 'motivation_id'
+			cmd = motivation_id_cmd
 		when 'accomplished_id'
 			cmd = accomplished_id_cmd
 		end
@@ -125,47 +127,73 @@ end
 	end
 
 #-------------------------------DRIVER CODE-------------------------------#
-# create_user(db, "Caitlin Johnson", 30, 60608, 1, 3)
-# create_user(db, "Ryan Johnson", 35, 77006, 2, 1)
-# create_user(db, "Grayson Bagwell", 30, 60608, 3, 2)
-# create_user(db, "Becky Brand", 56, 77006, 4, 2)
+# create_user(db, "Caitlin Johnson", 30, 60608, "caitlinlikesyou", "Dollywood", "Dolly Parton is a goddess.", 37863, 3, 3)
+# create_user(db, "Ryan Johnson", 35, 77006, "rynomite", "Dunk a basketball", "like Lebron", 70123, 2, 1)
+# create_user(db, "Grayson Bagwell", 30, 60608, "free_ghb", "Scuba dive", "Look at sharks!", 96701, 4, 2)
+# create_user(db, "Becky Brand", 56, 77006, "beckswithgoodhair", "eat a burger", "why not", 38936, 1, 2)
 # create_accomplished(db, "I did it! Heck yeah!")
 # create_accomplished(db, "I'm getting so close!")
 # create_accomplished(db, "I'm planning on starting tomorrow! Promise!")
-
-# create_summergoal(db, "Dollywood", "Dolly Parton is a goddess.", 96701, 0)
-# create_summergoal(db, "Scuba Dive", "Fight a shark!", 00123, 0)
-# create_summergoal(db, "Dunk a Basketball", "Like Lebron.", 44101, 0)
-# create_summergoal(db, "Eat a burger", "why not", 90210, 0)
+# create_motivation(db, "Comparing myself to others on social media.")
+# create_motivation(db, "For my family.")
+# create_motivation(db, "For my health and mental well-being")
+# create_motivation(db, "For an essential sense of self and happiness.")
 # delete_user(db, "Becky Brand")
-display_user(db)
+# display_user(db)
 
 #-------------------------------UI-------------------------------#
-# puts "Let's set our Summer Goal!"
-# puts "To add a user: enter 'user'."
-# puts "To create a summer goal: enter 'goal'."
-# puts "To list social media accounts: enter 'social'."
-# puts "To update a user's information: enter 'update'."
-# puts "To remove a user: enter 'delete'."
-# input = gets.chomp
+puts "Let's set our Summer Goal!"
+puts "To add a user: enter 'user'."
+puts "To update a user's information: enter 'update'."
+puts "To remove a user: enter 'delete'."
+input = gets.chomp
 
-# activity_id = 0
-# 	social_id = 0
 
-# case input
+case input
+when 'user'
 
-# when 'user'
+	puts "User's name:"
+	user_name = gets.chomp
 
-# 	puts "User's name:"
-# 	username = gets.chomp
+	puts "User's age:"
+	user_age = gets.chomp.to_i
 
-# 	puts "User's age:"
-# 	userage = gets.chomp.to_i
+	puts "User's zipcode:"
+	user_zip = gets.chomp.to_i
 
-# 	puts "User's zipcode:"
-# 	userzip = gets.chomp.to_i
+	puts "User's Instagram username:"
+	user_instagram = gets.chomp
 
-# 
+	puts "What is your summer goal?"
+	user_goal = gets.chomp
+
+	puts "What comment do you have for your goal?"
+	user_comment = gets.chomp
+
+	puts "Where is your summer goal located? (zipcode)."
+	goal_zip = gets.chomp.to_i
+
+	puts "What is motivating you to complete this goal? \nChoose from: 'social media', 'family', 'health', 'happiness'."
+	user_motivation = gets.chomp
+		if user_motivation == "social media"
+			motivation_id = 1
+		elsif user_motivation == "family"
+			motivation_id = 2
+		elsif user_motivation == "health"
+			motivation_id = 3
+		elsif user_motivation == "happiness"
+			motivation_id = 4
+		else motivation_id = 5
+		end 
+
+	puts "On a scale of 1 to 3, with 1 being completely accomplished,\nhow far are you from reaching your goal?"
+	goal_reach = gets.chomp.to_i
+
+	create_user(db, user_name, user_age, user_zip, user_instagram, user_goal, user_comment, goal_zip, motivation_id, goal_reach)
+end
+
+display_user(db)
+
 
 
 
