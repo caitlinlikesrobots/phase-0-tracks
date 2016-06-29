88355c4d519cea7e1265c9b30f45453e1b3b8889
caitlinlikesrobots <caitlin.annette.johnson@gmail.com>
2016-06-29 14:54:28 -0500

    Update DOM manipulations

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,2 +1,31 @@
-console.log("The script is running!");
+console.log("The script is running!"); 
 
+
+// function addBlueBackground() {
+//   var docBackground = document.getElementsByTagName("p");
+//   var docBackground = document.body.style.backgroundColor = "blue";
+// }
+
+// addBlueBackground();
+
+// create functioning side bar with js
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "#5900b3";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "#99ff99";
+}
+
+function addPurple(event) {
+  console.log("mouse over happened!");
+  event.target.style.backgroundColor = "purple"
+
+}
+
+var button = document.getElementsByTagName("button");
+button[0].addEventListener("mouseover", addPurple);
