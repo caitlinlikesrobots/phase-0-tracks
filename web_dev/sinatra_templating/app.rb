6fc85859d5f1397a55ce0e9330459901b9ff6481
caitlinlikesrobots <caitlin.annette.johnson@gmail.com>
2016-07-07 17:44:26 -0500

    Create table for mascot suggestions

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -25,3 +25,20 @@ post '/students' do
 end
 
 # add static resources
+get '/students/mascot' do
+    erb :mascot
+end
+
+get '/students/mascot/mascot_ideas' do
+  @mascots = db.execute("SELECT * FROM mascots")
+  erb :mascot_ideas
+end
+
+post '/students/mascot' do 
+  db.execute("CREATE TABLE IF NOT EXISTS mascots (mascot_ideas VARCHAR(255))")
+  db.execute("INSERT INTO mascots (mascot_ideas) VALUES (?)", [params['mascot_ideas']])
+  redirect '/students/mascot/mascot_ideas'
+end
+
+
+  
